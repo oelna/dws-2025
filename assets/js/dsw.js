56809098116527deafc61a7f2dec8e2747577b7a
Arno Richter <mail@arnorichter.de>
2025-12-18 12:19:12 +0100

    letzter stand vor navigation umbau

---
+++ assets/js/dsw.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/* light/dark mode toggle persistence, https://www.smashingmagazine.com/2024/03/setting-persisting-color-scheme-preferences-css-javascript/ */
+const defaultMode = window.matchMedia?.("(prefers-color-scheme: dark)").matches ? 'dark' : 'light';
+const modeToggle = document.querySelector('[name="toggle-color-scheme"]');
+let manualMode = localStorage.getItem('manual-light-dark');
+
+if (manualMode) {
+	modeToggle.checked = (manualMode == 'dark') ? true : false;
+	document.documentElement.setAttribute('data-mode', manualMode);
+} else {
+	if (defaultMode == 'dark') {
+		modeToggle.checked = true;
+		document.documentElement.style.setProperty('--color-scheme', 'dark');
+		document.documentElement.setAttribute('data-mode', 'dark');
+	} else {
+		modeToggle.checked = false;
+		document.documentElement.style.setProperty('--color-scheme', 'light');
+		document.documentElement.setAttribute('data-mode', 'light');
+	}
+}
+
+modeToggle?.addEventListener('change', function (event) {
+	manualMode = event.target.checked ? 'dark' : 'light';
+	localStorage.setItem('manual-light-dark', manualMode);
+	document.documentElement.style.setProperty('--color-scheme', manualMode);
+	document.documentElement.setAttribute('data-mode', manualMode);
+
+	// console.log('manually set mode to', manualMode);
+});
--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 
+/* use theme CSS file for the site */
 add_action('enqueue_block_assets', function () {
 	$theme = wp_get_theme();
 
@@ -9,4 +10,28 @@ add_action('enqueue_block_assets', function () {
 		['wp-block-library', 'global-styles'],
 		$theme->get('Version')
 	);
+
+	wp_enqueue_script(
+		'dsw-main-js', 
+		get_theme_file_uri('assets/js/dsw.js'), 
+		null, 
+		$theme->get('Version'), 
+		true
+	);
 }, 20);
+
+/* use theme CSS file in the editor UI */
+add_action('after_setup_theme', function () {
+	add_theme_support('editor-styles');
+	add_editor_style('assets/css/main.css');
+});
+
+function dws_relative_url($url) {
+	if (is_admin() || is_feed()) return $url;
+	return wp_make_link_relative($url);
+}
+
+add_filter('the_permalink', 'dws_relative_url');
+add_filter('wp_get_attachment_url', 'dws_relative_url');
+add_filter('script_loader_src', 'dws_relative_url');
+add_filter('style_loader_src', 'dws_relative_url');
